3b70f44b56269b6713b50e57cfcc9fed31ff5ad7
ClariS <1457715339@qq.com>
2021-05-20 15:03:53 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,8 @@
 <template>
-  <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">你好</Button>
+  <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
 </template>
 
 <script lang="ts">
