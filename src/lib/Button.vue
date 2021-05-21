7e3717959367d20be88e58da9b6c47945f9a154f
ClariS <1457715339@qq.com>
2021-05-21 17:19:41 +0800

    loading css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="wheel-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="wheel-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -27,9 +28,14 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
     const {theme, size, level} = props;
+    console.log(props);
     const classes = computed(() => {
       return {
         [`wheel-theme-${theme}`]: theme,
@@ -187,6 +193,27 @@ $grey: grey;
       cursor: not-allowed;
       color: $grey;
     }
+  }
+
+  > .wheel-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: wheel-spin 1s infinite linear;
+  }
+}
+
+@keyframes wheel-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
   }
 }
 </style>
