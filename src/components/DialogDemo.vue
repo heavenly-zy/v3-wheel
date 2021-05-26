469b0ae566953b5a998a4793462e80e742442bd9
ClariS <1457715339@qq.com>
2021-05-26 20:07:15 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -11,12 +11,15 @@
       <div>hello2</div>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
-import {ref} from 'vue';
+import {h, ref} from 'vue';
+import {openDialog} from '../lib/openDialog';
 
 export default {
   name: 'DialogDemo',
@@ -31,7 +34,21 @@ export default {
     };
     const f2 = () => {
     };
-    return {x, toggle, f1, f2};
+    const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        closeOnClickOverlay: true,
+        ok() {
+          console.log('ok');
+          return false;
+        },
+        cancel() {
+          console.log('cancel');
+        },
+      });
+    };
+    return {x, toggle, f1, f2, showDialog};
   },
 };
 </script>
