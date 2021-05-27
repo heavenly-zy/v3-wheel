c248ce8cc195260ae2b8c31d32f438c0dfb303de
ClariS <1457715339@qq.com>
2021-05-27 16:17:47 +0800

    渲染 Tabs 内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,8 +1,8 @@
 <template>
   <h1>示例1</h1>
   <Tabs>
-    <Tab title="导航1"></Tab>
-    <Tab title="导航2"></Tab>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 
