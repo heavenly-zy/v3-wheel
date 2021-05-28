45183292f6d3fd23fb3d9ae2f2df577c5da4d0b4
ClariS <1457715339@qq.com>
2021-05-28 15:32:34 +0800

    优化 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
            :key="index"
            @click="select(t)"
            :class="{selected: t===selected}"
-           :ref="el => {if(el) navItems[index] = el}"
+           :ref="el => {if(t===selected) {selectedItem = el}}"
       >{{ t }}
       </div>
       <div class="wheel-tabs-nav-indicator" ref="indicator"></div>
@@ -30,16 +30,14 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.find(div => div.classList.contains('selected'));
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = `${width}px`;
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = `${left}px`;
     };
@@ -62,7 +60,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title);
     };
-    return {defaults, titles, current, select, navItems, indicator, container};
+    return {defaults, titles, current, select, selectedItem, indicator, container};
   },
 };
 </script>
