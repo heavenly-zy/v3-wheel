0d2aa453282e116e1c99aad3015a02418e4b5b03
ClariS <1457715339@qq.com>
2021-06-09 20:34:35 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,5 +1,15 @@
 <template>
-	安装
+	<article>
+		<h1>安装</h1>
+		<p>打开终端运行下列命令：</p>
+		<pre><code>npm install v3-wheel</code></pre>
+		<p>或</p>
+		<pre><code>yarn add v3-wheel</code></pre>
+
+		<p>
+			下一节：<a href="#/doc/get-started">开始使用</a>
+		</p>
+	</article>
 </template>
 
 <script>
