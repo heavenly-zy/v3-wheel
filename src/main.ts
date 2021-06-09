04fdf3cca60aa5188fba9192f6974d521625991e
ClariS <1457715339@qq.com>
2021-06-09 17:16:13 +0800

    使用本地的 svg.js

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./index.scss";
 import {createApp} from "vue";
 import App from "./App.vue";
 import {router} from "./router"; // global css
+import './lib/svg.js'
 
 const app = createApp(App);
 app.use(router);
