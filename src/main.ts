e9dd35a7e9641cc5e15a21a1434f05e5278b828f
ClariS <1457715339@qq.com>
2021-06-09 20:38:51 +0800

    引入 github 样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import "./lib/wheel.scss"
-import "./index.scss";
-import {createApp} from "vue";
-import App from "./App.vue";
-import {router} from "./router"; // global css
-import './lib/svg.js'
+import './lib/wheel.scss';
+import './index.scss';
+import {createApp} from 'vue';
+import App from './App.vue';
+import {router} from './router'; // global css
+import './lib/svg.js';
+import 'github-markdown-css';
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
