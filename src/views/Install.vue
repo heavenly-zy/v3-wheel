344c9b59594bcc001e5ae45a6bb6cd875d911f6b
ClariS <1457715339@qq.com>
2021-06-10 10:37:51 +0800

    支持 markdown 文件

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,21 +1,19 @@
 <template>
-	<article class="markdown-body">
-		<h1>安装</h1>
-		<p>打开终端运行下列命令：</p>
-		<pre><code>npm install v3-wheel</code></pre>
-		<p>或</p>
-		<pre><code>yarn add v3-wheel</code></pre>
-
-		<p>
-			下一节：<a href="#/doc/get-started">开始使用</a>
-		</p>
+	<article class="markdown-body" v-html="md">
 	</article>
 </template>
 
-<script>
+<script lang="ts">
+import md from '../markdown/install.md';
+
 export default {
 	name: 'Install',
-}
+	data() {
+		return {
+			md,
+		};
+	},
+};
 </script>
 
 <style scoped>
