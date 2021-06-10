3b3b9e7dc5d958eb13a9619c2c0951cecb16f3f2
ClariS <1457715339@qq.com>
2021-06-10 20:09:00 +0800

    markdown 文件改用静态加载的方式

--- src/components/MarkDown.vue
+++ src/components/MarkDown.vue
@@ -3,27 +3,14 @@
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
 
 export default {
 	name: 'MarkDown',
 	props: {
-		path: {
+		content: {
 			type: String,
 			required: true,
 		},
-	},
-	setup(props) {
-		const content = ref<string>('');
-		// 这里用到了动态 import
-		import(props.path).then(result => {
-			content.value = result.default;
-		});
-		return {content};
-	},
+	}
 };
 </script>
-
-<style scoped>
-
-</style>
